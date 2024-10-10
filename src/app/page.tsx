83556540439af27de943403ba3cc6025718f9c95
shobhitsrivastava2023 <shobhit.229302512@muj.manipal.edu>
2024-10-11 00:50:06 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
+'use client'
+
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const Mergedata = [
+    { "time": 3500, "arrayLength": 46 },
+    { "time": 7000, "arrayLength": 66 },
+    { "time": 20400, "arrayLength": 176 },
+    { "time": 117400, "arrayLength": 224 },
+    { "time": 26400, "arrayLength": 371 },
+    { "time": 36800, "arrayLength": 500 },
+    { "time": 43300, "arrayLength": 566 },
+    { "time": 44200, "arrayLength": 587 },
+    { "time": 46400, "arrayLength": 612 },
+    { "time": 46500, "arrayLength": 615 },
+    { "time": 55000, "arrayLength": 718 },
+    { "time": 62900, "arrayLength": 810 },
+    { "time": 69300, "arrayLength": 867 },
+    { "time": 74000, "arrayLength": 952 },
+    { "time": 76300, "arrayLength": 970 }
+  ]
+
+  const quicksort = [
+    { "time": 300, "arrayLength": 6 },
+    { "time": 8100, "arrayLength": 166 },
+    { "time": 27000, "arrayLength": 196 },
+    { "time": 14200, "arrayLength": 251 },
+    { "time": 13500, "arrayLength": 261 },
+    { "time": 14600, "arrayLength": 272 },
+    { "time": 30900, "arrayLength": 356 },
+    { "time": 19000, "arrayLength": 357 },
+    { "time": 27000, "arrayLength": 440 },
+    { "time": 647000, "arrayLength": 473 },
+    { "time": 46000, "arrayLength": 691 },
+    { "time": 44500, "arrayLength": 710 },
+    { "time": 49300, "arrayLength": 806 },
+    { "time": 53700, "arrayLength": 843 },
+    { "time": 60900, "arrayLength": 914 }
+  ];
+
+
+  const randomizedData = [
+    { "time": 1800, "arrayLength": 23 },
+    { "time": 15800, "arrayLength": 152 },
+    { "time": 35800, "arrayLength": 336 },
+    { "time": 42800, "arrayLength": 380 },
+    { "time": 44700, "arrayLength": 407 },
+    { "time": 56400, "arrayLength": 442 },
+    { "time": 60400, "arrayLength": 490 },
+    { "time": 65200, "arrayLength": 550 },
+    { "time": 77800, "arrayLength": 659 },
+    { "time": 561400, "arrayLength": 574 },
+    { "time": 180500, "arrayLength": 739 },
+    { "time": 93200, "arrayLength": 753 },
+    { "time": 391200, "arrayLength": 826 },
+    { "time": 131400, "arrayLength": 953 },
+    { "time": 139400, "arrayLength": 976 }
+  ];
+
+  return (
+    <div className="container mx-auto p-4 bg-white">
+      <h1 className="text-2xl font-bold mb-4  text-black">Randomized Sort Performance Visualization</h1>
+      <div className="w-full h-[500px]">
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={randomizedData}
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis type="number" dataKey="arrayLength" name="Array Length" unit=" elements" />
+            <YAxis type="number" dataKey="time" name="Time" unit=" ns" />
+            <Tooltip />
+            <Line type="monotone" dataKey="time" stroke="#8884d8" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
